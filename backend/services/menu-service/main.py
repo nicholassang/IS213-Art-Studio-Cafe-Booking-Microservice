1a6b4c1e5f51adf81c9c559e6ee2600cafaf1cea
lexinphun2024-debug <lexin.phun.2024@smu.edu.sg>
2026-04-04 16:18:12 +0800

    move the supabase to env file

--- backend/services/menu-service/main.py
+++ backend/services/menu-service/main.py
@@ -1,22 +1,29 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:5174",   
+        "http://127.0.0.1:5174",  
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
-
 # Supabase connection
-SUPABASE_URL = "https://blgtzrznellrbuptcogs.supabase.co"  
-SUPABASE_KEY = "sb_publishable_VabQeIqtF9gGYouJFlyYhA_MaUdg9L7"                      
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # Health check
